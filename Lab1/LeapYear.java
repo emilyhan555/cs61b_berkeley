8b44e1f23faad9f9d3609865fd6c55c2c33654a4
emily_han <emily@LAPTOP-EUNE05KI.local>
2020-04-28 00:08:49 -0500

    completed LeapYear.java

--- Lab1/LeapYear.java
+++ Lab1/LeapYear.java
@@ -1,1 +1,28 @@
+public class LeapYear {
+    /** Create a method that can tell if a Year is a leap year or not.  */
+    public static boolean isLeapYear(final int year){
+        if ((year%400 == 0) 
+            || (year%4==0 && year%100 != 0) ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
+    public static void main(final String[] args) {
+        int year;
+        int[] a = {2000,1999,2004,2100,2003};
+        for (int i = 0; i< a.length; i++){
+                year = a[i];
+                isLeapYear(year);
+        
+        if (isLeapYear(year) == true) {
+            System.out.println(year+ " is a leap year.");
+        }
+        else {
+            System.out.println(year+" is not a leap year.");
+        }
+    }
+}
+}
