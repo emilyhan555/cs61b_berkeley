7222ff72cde2ec0d9d35f6a50dddd72cff6a9b13
emily_han <emily@LAPTOP-EUNE05KI.local>
2020-05-16 23:36:50 -0500

    project 1b finished

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,10 +1,12 @@
 public class OffByN implements CharacterComparator {
     public int NItem;
 
+    /** Constructor: create objects whose equalChars method return true for characters that are off by N. */
     public OffByN(int N){
         NItem = N;
     }
 
+    /** If the given x & y off by NItem, then return true. */
     public boolean equalChars(char x, char y){
         if (Math.abs(x-y) == NItem){
             return true;
