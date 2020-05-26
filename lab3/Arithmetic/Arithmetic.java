ac1182b7e07d96d8b52552a095ff7a13470fb8d6
emily_han <emily@LAPTOP-EUNE05KI.local>
2020-05-26 01:48:56 -0500

    lab3 completed

--- lab3/Arithmetic/Arithmetic.java
+++ lab3/Arithmetic/Arithmetic.java
@@ -1,1 +1,15 @@
 
+public class Arithmetic {
+    /** Computes product of two ints */
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+
+    /** Computes sum of two ints (incorrectly) */
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
+//© 2020 GitHub, Inc.
